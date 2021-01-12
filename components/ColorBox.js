fb97bba02b061fe0c68f984bd690fdd37f18b4e9
Alladin Daher <alladin.daher@hotmail.com>
2021-01-12 19:43:32 +0100

    Update ColorBox.js
    added flatlist for rendering

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -1,42 +1,33 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const ColorBox = props => {
-  const colorStyle = {
-    backgroundColor: props.hexCode,
+const ColorBox = ({ colorName, hexCode }) => {
+  const boxColor = {
+    backgroundColor: hexCode,
   };
-
   const textStyle = {
     color:
-      parseInt(props.hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+      parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1
         ? 'black'
         : 'white',
   };
-
   return (
-    <View style={[styles.box, colorStyle]}>
+    <View style={[styles.colours, boxColor]}>
       <Text style={[styles.text, textStyle]}>
-        {props.colorName} {props.hexCode}
+        {colorName}: {hexCode}
       </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  box: {
-    padding: 10,
-    borderRadius: 3,
-    justifyContent: 'center',
+  colours: {
     alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
     marginBottom: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.3,
-    shadowRadius: 1,
-    elevation: 2,
   },
   text: {
-    fontWeight: 'bold',
     color: 'white',
   },
 });
